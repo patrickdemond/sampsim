eb19d62bc893a6682b343d85695ab36357fcf04b
Patrick Emond <patrickdemond@gmail.com>
2013-09-27 15:55:04 -0400

    Finished setting up unit testing
    Now that the unit testing infrastructure is set up all that is left
    to do is implement the tests themselves using UnitTest++

---
+++ lib/local/sample/testing/test_arc_epi.cxx
@@ -0,0 +1,20 @@
+/*=========================================================================
+
+  Program:  sampsim
+  Module:   test_sample_random.cxx
+  Language: C++
+
+=========================================================================*/
+//
+// .SECTION Description
+// Unit tests for the sample::random class
+//
+
+#include "UnitTest++.h"
+
+int main( const int argc, const char** argv ) { return UnitTest::RunAllTests(); }
+
+TEST( test_sample_random_ )
+{
+  CHECK( true );
+}
