0c4aee82bebd7723127e0b94b5cf80f4eb8a380a
Patrick Emond <patrickdemond@gmail.com>
2013-11-29 14:37:26 -0500

    Writing API documentation

--- lib/local/building_tree.h
+++ lib/local/building_tree.h
@@ -43,10 +43,14 @@ namespace sampsim
   {
   private:
     /**
-     * TODO: document
+     * @struct node
+     * @brief An internal struct for handling tree nodes
      */
     struct node
     {
+      /**
+       * Constructor
+       */
       node( node* parent = NULL )
       {
         this->parent = parent;
@@ -56,6 +60,12 @@ namespace sampsim
         this->right = NULL;
       }
 
+      /**
+       * Returns the node's median value
+       * 
+       * This will depend on the depth of the node since the axis the median is calculated
+       * from alternates between the x and y axes.
+       */
       double get_median()
       {
         if( NULL == this->building )
@@ -65,33 +75,86 @@ namespace sampsim
         return 0 == depth % 2 ? c.x : c.y;
       }
 
+      /**
+       * The node's parent node (null for the root node)
+       */
       node* parent;
+      
+      /**
+       * The node's depth
+       */
       long long int depth;
+      
+      /**
+       * The building found at the node's position
+       */
       sampsim::building* building;
+      
+      /**
+       * The node's left (negative axis direction) child node
+       */
       node *left;
+
+      /**
+       * The node's right (positive axis direction) child node
+       */
       node *right;
     };
 
   public:
+    /**
+     * Constructor
+     */
     building_tree( building_list_type );
     
+    /**
+     * Destructor
+     */
     ~building_tree();
 
+    /**
+     * Returns the building which is located nearest to the given coordinate
+     */
     building* find_nearest( coordinate );
 
+    /**
+     * Provides a string representation of the tree
+     */
     std::string to_string() { return building_tree::to_string( this->root_node ); }
 
   private:
+    /**
+     * A recursive function used to create a 2d-tree of buildings
+     */
     static node* build( building_list_type, node* parent_node = NULL );
 
+    /**
+     * A recursive function that deletes the node an all of its children.
+     */
     static void destroy( node* );
     
+    /**
+     * A recursive function that returns the nearest node in the tree
+     * 
+     * This is used by the find_nearest() method.
+     */
     static node* find_nearest_node( node*, coordinate, double& );
 
+    /**
+     * A recursive function that searches for the nearest leaf-node
+     * 
+     * This is used by the find_nearest_node() method.
+     */
     static node* find_leaf( node*, double x, double y );
 
+    /**
+     * A recursive function used to create a string representation of a tree
+     */
     static std::string to_string( node* );
 
+    /**
+     * The tree's root node
+     */
     node *root_node;
   };
 }
