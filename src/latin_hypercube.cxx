6a28d797ad5816e2f3d958c842692987b0981be7
Patrick Emond <patrickdemond@gmail.com>
2016-01-17 23:06:34 -0500

    Adding new "maximums" option to latin_hypercube
    Each dimension can now be separated into any number of divisions required.

--- src/latin_hypercube.cxx
+++ src/latin_hypercube.cxx
@@ -27,14 +27,17 @@ int main( const int argc, const char** argv )
   opts.add_option( 's', "seed", "1", "Seed used by the random generator" );
   opts.add_option( 'd', "dims", "2", "The number of dimensions to generate" );
   opts.add_option( 'p', "points", "10", "The number of points to generate" );
-  opts.add_flag( 'i', "index", "Whether to output indeces instead of values between 0 and 1" );
-  opts.add_flag( 't', "transpose", "Whether to transpose the output" );
+  opts.add_option( 'm', "maximums", vector< string >(), "The maximum number of divisions for each dimension" );
+  opts.add_flag( 'i', "index0", "Whether to output 0-based indeces instead of values between 0 and 1" );
+  opts.add_flag( 'j', "index1", "Whether to output 1-based indeces instead of values between 0 and 1" );
 
   try
   {
     opts.set_arguments( argc, argv );
     if( opts.process() )
     {
+      status = EXIT_SUCCESS;
+
       // now either show the help or run the application
       if( opts.get_flag( "help" ) )
       {
@@ -45,24 +48,20 @@ int main( const int argc, const char** argv )
         int seed = opts.get_option_as_int( "seed" );
         int dims = opts.get_option_as_int( "dims" );
         int points = opts.get_option_as_int( "points" );
-        bool index = opts.get_flag( "index" );
-        bool transpose = opts.get_flag( "transpose" );
+        vector< int > maximums = opts.get_option_as_int_list( "maximums" );
+        bool index0 = opts.get_flag( "index0" );
+        bool index1 = opts.get_flag( "index1" );
 
         std::vector<double> data( dims*points, 0.0 );
         latin_center( dims, points, &seed, data.data() );
 
-        if( transpose )
+        // make sure the number of maximums (if any) matches the number of dimensions
+        if( 0 < maximums.size() && maximums.size() != dims )
         {
-          for( int dim = 0; dim < dims; dim++ )
-          {
-            for( int pnt = 0; pnt < points; pnt++ )
-            {
-              if( 0 < pnt ) cout << ",";
-              if( index ) cout << round( points * ( data[pnt + points*dim] - 1/(2.0*points) ) );
-              else cout << data[pnt + points*dim];
-            }
-            cout << endl;
-          }
+          cerr << "ERROR: The number of maximum divisions does not equal the number of dimensions." << endl
+               << "If you wish to set the maximum divisions please make sure to provide a comma-separated "
+               << "list with one number per dimesion indicated by the \"dims\" option." << endl;
+          status = EXIT_FAILURE;
         }
         else
         {
@@ -70,16 +69,29 @@ int main( const int argc, const char** argv )
           {
             for( int dim = 0; dim < dims; dim++ )
             {
+              // get the point and convert if the maximum number of divisions for this dimension is set
+              double val = data[pnt + points*dim];
+              double factor = points;
+              if( 0 < maximums.size() && maximums[dim] != points )
+              {
+                double m = static_cast<double>( maximums[dim] );
+                double p = static_cast<double>( points );
+                val = ceil( (val + 1/(2*p)) * m - 1E-10)/m - 1/(2*m);
+                factor = m;
+              }
+
               if( 0 < dim ) cout << ",";
-              if( index ) cout << round( points * ( data[pnt + points*dim] - 1/(2.0*points) ) );
-              else cout << data[pnt + points*dim];
+              if( index0 || index1 )
+              {
+                int index = round( factor * ( val - 1/(2.0*factor) ) );
+                cout << ( index0 ? index : index+1 );
+              }
+              else cout << val;
             }
             cout << endl;
           }
         }
       }
-
-      status = EXIT_SUCCESS;
     }
   }
   catch( std::exception &e )
