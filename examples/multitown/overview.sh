4613b986379941feec38d2dd45cb6a435240f0e2
Patrick Emond <patrickdemond@gmail.com>
2017-01-22 13:09:36 -0500

    Updating scripts to work with new CSV file formats

--- examples/multitown/overview.sh
+++ examples/multitown/overview.sh
@@ -8,21 +8,21 @@ echo "town,pop,child,income,disease,income,disease"
 
 for i in {0..249}; do
   echo -n "$(($i+1))," &&
-  grep "^$i,[0-9]\+," vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
-  grep "^$i,[0-9]\+,child" vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
+  grep "^$i,[0-9]\+,[0-9]\+," vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
+  grep "^$i,[0-9]\+,[0-9]\+,child" vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
   grep "^$i,[0-9]\+," vanilla.flat.household.csv | sed -e "s/^\([0-9]\+\),.*,\([0-9.]\+\),0.000,[01]$/\2/" | awk '{s+=$1}END{print s/NR}' RS="\n" | tr '\n' ',' &&
-  grep "^$i,[0-9]\+,child,\(fe\)\?male,1" vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
+  grep "^$i,[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
   grep "^$i,[0-9]\+," vanilla.bbb.household.csv | sed -e "s/^\([0-9]\+\),.*,\([0-9.]\+\),0.000,[01]$/\2/" | awk '{s+=$1}END{print s/NR}' RS="\n" | tr '\n' ',' &&
-  grep "^$i,[0-9]\+,child,\(fe\)\?male,1" vanilla.bbb.individual.csv | wc -l; 
+  grep "^$i,[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" vanilla.bbb.individual.csv | wc -l; 
 done
 
 echo -n "overall," &&
-grep "^[0-9]\+,[0-9]\+," vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
-grep "^[0-9]\+,[0-9]\+,child" vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
+grep "^[0-9]\+,[0-9]\+,[0-9]\+," vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
+grep "^[0-9]\+,[0-9]\+,[0-9]\+,child" vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
 grep "^[0-9]\+,[0-9]\+," vanilla.flat.household.csv | sed -e "s/^\([0-9]\+\),.*,\([0-9.]\+\),0.000,[01]$/\2/" | awk '{s+=$1}END{print s/NR}' RS="\n" | tr '\n' ',' &&
-grep "^[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
+grep "^[0-9]\+,[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" vanilla.flat.individual.csv | wc -l | tr '\n' ',' &&
 grep "^[0-9]\+,[0-9]\+," vanilla.bbb.household.csv | sed -e "s/^\([0-9]\+\),.*,\([0-9.]\+\),0.000,[01]$/\2/" | awk '{s+=$1}END{print s/NR}' RS="\n" | tr '\n' ',' &&
-grep "^[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" vanilla.bbb.individual.csv | wc -l; 
+grep "^[0-9]\+,[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" vanilla.bbb.individual.csv | wc -l; 
 
 echo ""
 echo "mx=1;my=1"
@@ -30,18 +30,18 @@ echo "town,pop,child,income,disease,income,disease"
 
 for i in {0..249}; do
   echo -n "$(($i+1))," &&
-  grep "^$i,[0-9]\+," popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
-  grep "^$i,[0-9]\+,child" popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
+  grep "^$i,[0-9]\+,[0-9]\+," popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
+  grep "^$i,[0-9]\+,[0-9]\+,child" popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
   grep "^$i,[0-9]\+," popdens.flat.household.csv | sed -e "s/^\([0-9]\+\),.*,\([0-9.]\+\),0.000,[01]$/\2/" | awk '{s+=$1}END{print s/NR}' RS="\n" | tr '\n' ',' &&
-  grep "^$i,[0-9]\+,child,\(fe\)\?male,1" popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
+  grep "^$i,[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
   grep "^$i,[0-9]\+," popdens.bbb.household.csv | sed -e "s/^\([0-9]\+\),.*,\([0-9.]\+\),0.000,[01]$/\2/" | awk '{s+=$1}END{print s/NR}' RS="\n" | tr '\n' ',' &&
-  grep "^$i,[0-9]\+,child,\(fe\)\?male,1" popdens.bbb.individual.csv | wc -l; 
+  grep "^$i,[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" popdens.bbb.individual.csv | wc -l; 
 done
 
 echo -n "overall," &&
-grep "^[0-9]\+,[0-9]\+," popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
-grep "^[0-9]\+,[0-9]\+,child" popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
+grep "^[0-9]\+,[0-9]\+,[0-9]\+," popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
+grep "^[0-9]\+,[0-9]\+,[0-9]\+,child" popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
 grep "^[0-9]\+,[0-9]\+," popdens.flat.household.csv | sed -e "s/^\([0-9]\+\),.*,\([0-9.]\+\),0.000,[01]$/\2/" | awk '{s+=$1}END{print s/NR}' RS="\n" | tr '\n' ',' &&
-grep "^[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
+grep "^[0-9]\+,[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" popdens.flat.individual.csv | wc -l | tr '\n' ',' &&
 grep "^[0-9]\+,[0-9]\+," popdens.bbb.household.csv | sed -e "s/^\([0-9]\+\),.*,\([0-9.]\+\),0.000,[01]$/\2/" | awk '{s+=$1}END{print s/NR}' RS="\n" | tr '\n' ',' &&
-grep "^[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" popdens.bbb.individual.csv | wc -l; 
+grep "^[0-9]\+,[0-9]\+,[0-9]\+,child,\(fe\)\?male,1" popdens.bbb.individual.csv | wc -l; 
--- examples/multitown/samples.sh
+++ examples/multitown/samples.sh
@@ -6,24 +6,24 @@
 find -type d | grep sample | sed -e "s#\./\(.*\)_sample#\1#" | while read sample; do
   echo $sample
   echo "mean,mean,stdev,mean,stdev,mean,stdev,,mean,mean,stdev,mean,stdev,mean,stdev"
-  grep "^child" vanilla.flat.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/vanilla.flat.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/vanilla.flat.10.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/vanilla.flat.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^child count:" vanilla.flat.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/vanilla.flat.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/vanilla.flat.10.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/vanilla.flat.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
   echo -n "," &&
-  grep "^child" popdens.flat.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/popdens.flat.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/popdens.flat.10.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/popdens.flat.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" &&
+  grep "^child count:" popdens.flat.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/popdens.flat.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/popdens.flat.10.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/popdens.flat.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" &&
 
-  grep "^child" vanilla.bbb.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/vanilla.bbb.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/vanilla.bbb.10.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/vanilla.bbb.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^child count:" vanilla.bbb.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/vanilla.bbb.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/vanilla.bbb.10.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/vanilla.bbb.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
   echo -n "," &&
-  grep "^child" popdens.bbb.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/popdens.bbb.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/popdens.bbb.10.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child" ${sample}_sample/popdens.bbb.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" &&
+  grep "^child count:" popdens.bbb.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/popdens.bbb.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/popdens.bbb.10.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
+  grep "^sampled child count:" ${sample}_sample/popdens.bbb.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" &&
   echo "";
 done
