17f1951392e54a38a438b5d321e9aa81c99f7529
Patrick Emond <patrickdemond@gmail.com>
2017-02-12 14:21:30 -0500

    Adding value for 0 and empty indeces to histogram

--- examples/multitown/histogram.sh
+++ examples/multitown/histogram.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
 function histogram {
-  cat $1.s*.individual.csv |
-    grep "^[0-9]\+,[0-9]\+,[0-9]\+," | # only include data
-    sed -e "s#^[0-9]\+,[0-9]\+,\([0-9]\+\),.*#\1#" | # only show individual index
-    sort -g | # sort individual index numerically
-    uniq -c | # count the number of times an index shows
-    sed -e "s# \+\([0-9]\+\) .*#\1#" | # remove the individual index
-    sort -g | # sort the counts numerically
-    uniq -c | # count the number of times a count shows
-    sed -e "s# \+\([0-9]\+\) \([0-9]\+\)#\2,\1#" # show "times,count"
+  echo -n "0,"
+  echo "`grep "child count" $2.txt |
+         sed -e "s/child count: [0-9]\+ diseased of \([0-9]\+\) total.*/\1/"` - \
+        `cat $1/$2.$3.s*.individual.csv |
+         grep "^[0-9]\+,[0-9]\+,[0-9]\+," |
+         sed -e "s#^[0-9]\+,[0-9]\+,\([0-9]\+\),.*#\1#" |
+         sort -g | uniq | wc -l`" | bc
+
+  index=1
+  for line in `
+    cat $1/$2.$3.s*.individual.csv |
+      grep "^[0-9]\+,[0-9]\+,[0-9]\+," | # only include data
+      sed -e "s#^[0-9]\+,[0-9]\+,\([0-9]\+\),.*#\1#" | # only show individual index
+      sort -g | # sort individual index numerically
+      uniq -c | # count the number of times an index shows
+      sed -e "s# \+\([0-9]\+\) .*#\1#" | # remove the individual index
+      sort -g | # sort the counts numerically
+      uniq -c | # count the number of times a count shows
+      sed -e "s# \+\([0-9]\+\) \([0-9]\+\)#\2,\1#" # show "times,count"
+  `; do
+    # print value of 0 for any missing index
+    current=`echo $line | sed -e "s/\([^,]\),.*/\1/"`
+    while [ $current -ne $index ]; do
+      echo $index,0
+      ((index++))
+    done
+    echo $line
+    ((index++))
+  done
 }
 
 for sampler in *_sample; do
   for pop in vanilla popdens; do
-    for type in flat bbb; do
-      for size in 7 30; do
-        echo $sampler $pop $type $size
-        histogram $sampler/$pop.$type.$size
-        echo
-      done
+    for size in 7 30; do
+      echo $sampler $pop $size
+      histogram $sampler $pop.flat $size
+      echo
     done
   done
 done
