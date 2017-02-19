a975cec359d139cb0811c5b4a3eea44e14f6d245
Patrick Emond <patrickdemond@gmail.com>
2017-02-19 16:14:24 -0500

    Re-writing the samples.sh bash script

--- examples/multitown/samples.sh
+++ examples/multitown/samples.sh
@@ -3,23 +3,48 @@
 # Parses the simulation and outputs the mean and stdev prevalence results for each population and sample type.
 # Run this script after building the multitown simulation.
 
-find -type d | grep sample | sed -e "s#\./\(.*\)_sample#\1#" | while read sample; do
-  echo $sample
-  echo "mean,mean,stdev,mean,stdev,mean,stdev,,mean,mean,stdev,mean,stdev,mean,stdev"
-  grep "^child count:" vanilla.flat.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
-  grep "^sampled child count:" ${sample}_sample/vanilla.flat.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child count:" ${sample}_sample/vanilla.flat.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  echo -n "," &&
-  grep "^child count:" popdens.flat.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
-  grep "^sampled child count:" ${sample}_sample/popdens.flat.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child count:" ${sample}_sample/popdens.flat.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" &&
+for summary in *_sample/vanilla.flat.nr*07*.txt; do
+  # print the title
+  echo $summary | sed -e 's#\(.*\)_sample/.*\.nr-\?\([^-]\+\)\?\.[0-9][0-9]\.txt#"\1 \2"#'
 
-  grep "^child count:" vanilla.bbb.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
-  grep "^sampled child count:" ${sample}_sample/vanilla.bbb.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child count:" ${sample}_sample/vanilla.bbb.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  echo -n "," &&
-  grep "^child count:" popdens.bbb.txt | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ',' &&
-  grep "^sampled child count:" ${sample}_sample/popdens.bbb.7.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" | tr '\n' ',' &&
-  grep "^sampled child count:" ${sample}_sample/popdens.bbb.30.txt | sed -e "s#.*(prevalence \(.*\) (\(.*\)))#\1,\2#" &&
-  echo "";
+  for income in "flat" "bbb"; do
+    sum=${summary/flat/$income}
+    for resample in "nr" "r"; do
+      sum=${sum/nr/$resample}
+      for pop in "vanilla" "popdens"; do
+        sum=${sum/vanilla/$pop}
+
+        # print the population's true mean
+        if [ "nr" == $resample ]; then
+          filename=${pop}.${income}.txt
+          if [ -e $filename ]; then
+            grep "^child count:" $filename | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ','
+          else
+            echo -n 'na,'
+          fi
+        else
+          echo -n ','
+        fi
+        echo -n ','
+
+        for prevalence in "prevalence" "weighted prevalence"; do
+          for size in "07" "30"; do
+            sum=${sum/07/$size}
+            if [ -e $sum ]; then
+              grep "^sampled child count:" $sum |
+                sed -e "s#.*($prevalence \([^ ]\+\) (\([^)]\+\))).*#\1,\2#" |
+               tr '\n' ','
+            else
+              echo -n 'na,na,'
+            fi
+            echo -n ','
+          done
+          echo -n ','
+        done
+      done
+      echo
+    done
+  done
+  echo
+
 done
