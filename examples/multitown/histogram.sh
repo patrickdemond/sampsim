061b69543483e35afcab7e54ac286bbf444d8281
Patrick Emond <patrickdemond@gmail.com>
2017-02-19 12:10:20 -0500

    Removing unneeded configuration files from multitown example

--- examples/multitown/histogram.sh
+++ examples/multitown/histogram.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 function histogram {
+  echo $1 | sed -e "s#\(.*\)/\(.*\)\.flat\.\([^-]\+\)-\?\(.*\)\.\([0-9]\+\)#\1 \2 \3 \4 \5#"
   echo -n "0,"
-  echo "`grep "child count" $2.txt |
+  echo "`grep "^child count" $1.txt |
          sed -e "s/child count: [0-9]\+ diseased of \([0-9]\+\) total.*/\1/"` - \
-        `cat $1/$2.$3.s*.individual.csv |
+        `cat $1.s*.individual.csv |
          grep "^[0-9]\+,[0-9]\+,[0-9]\+," |
          sed -e "s#^[0-9]\+,[0-9]\+,\([0-9]\+\),.*#\1#" |
          sort -g | uniq | wc -l`" | bc
 
   index=1
-  for line in `
-    cat $1/$2.$3.s*.individual.csv |
+  lines=`cat $1.s*.individual.csv |
       grep "^[0-9]\+,[0-9]\+,[0-9]\+," | # only include data
       sed -e "s#^[0-9]\+,[0-9]\+,\([0-9]\+\),.*#\1#" | # only show individual index
       sort -g | # sort individual index numerically
@@ -20,7 +20,8 @@ function histogram {
       sort -g | # sort the counts numerically
       uniq -c | # count the number of times a count shows
       sed -e "s# \+\([0-9]\+\) \([0-9]\+\)#\2,\1#" # show "times,count"
-  `; do
+  `
+  for line in $lines; do
     # print value of 0 for any missing index
     current=`echo $line | sed -e "s/\([^,]\),.*/\1/"`
     while [ $current -ne $index ]; do
@@ -30,14 +31,10 @@ function histogram {
     echo $line
     ((index++))
   done
+  echo
 }
 
-for sampler in *_sample; do
-  for pop in vanilla popdens; do
-    for size in 7 30; do
-      echo $sampler $pop $size
-      histogram $sampler $pop.flat $size
-      echo
-    done
-  done
+for summary in */*flat.*.txt; do
+  name=`echo $summary | sed -e 's#\.txt$##'`
+  histogram $name
 done
