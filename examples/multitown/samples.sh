265053ce0057c0c884a563d68f95f9fa94468264
Patrick Emond <patrickdemond@gmail.com>
2017-02-20 14:02:19 -0500

    Refining the samples.sh bash script

--- examples/multitown/samples.sh
+++ examples/multitown/samples.sh
@@ -6,6 +6,9 @@
 for summary in *_sample/vanilla.flat.nr*07*.txt; do
   # print the title
   echo $summary | sed -e 's#\(.*\)_sample/.*\.nr-\?\([^-]\+\)\?\.[0-9][0-9]\.txt#"\1 \2"#'
+  echo ',,,"mx=0;my=0",,,,,,,,,,,,,,,"mx=1;my=1"'
+  echo ',true,,7,,,30,,,7,,,30,,,,true,,7,,,30,,,7,,,30,'
+  echo ',mean,,mean,stdev,,mean,stdev,,mean,stdev,,meand,stdev,,,mean,,mean,stdev,,mean,stdev,,mean,stdev,,meand,stdev'
 
   for income in "flat" "bbb"; do
     sum=${summary/flat/$income}
@@ -16,6 +19,7 @@ for summary in *_sample/vanilla.flat.nr*07*.txt; do
 
         # print the population's true mean
         if [ "nr" == $resample ]; then
+          echo -n $income,
           filename=${pop}.${income}.txt
           if [ -e $filename ]; then
             grep "^child count:" $filename | sed -e "s#.*(prevalence \(.*\))#\1#" | tr '\n' ','
@@ -23,7 +27,7 @@ for summary in *_sample/vanilla.flat.nr*07*.txt; do
             echo -n 'na,'
           fi
         else
-          echo -n ','
+          echo -n ',,'
         fi
         echo -n ','
 
@@ -39,7 +43,6 @@ for summary in *_sample/vanilla.flat.nr*07*.txt; do
             fi
             echo -n ','
           done
-          echo -n ','
         done
       done
       echo
