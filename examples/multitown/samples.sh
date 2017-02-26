442e7eafd36d985972a4aac49e71efbdb5fe295a
Patrick Emond <patrickdemond@gmail.com>
2017-02-26 12:18:54 -0500

    Modifying samples.sh script

--- examples/multitown/samples.sh
+++ examples/multitown/samples.sh
@@ -5,17 +5,15 @@
 
 for summary in *_sample/vanilla.flat.nr*07*.txt; do
   # print the title
-  echo $summary | sed -e 's#\(.*\)_sample/.*\.nr-\?\([^-]\+\)\?\.[0-9][0-9]\.txt#"\1 \2"#'
+  echo -n $summary | sed -e 's#\(.*\)_sample/.*\.nr-\?\([^-]\+\)\?\.[0-9][0-9]\.txt#"\1 \2"#'
   echo ',,,"mx=0;my=0",,,,,,,,,,,,,,,"mx=1;my=1"'
+  echo ',,,"unweighted",,,,,,"weighted",,,,,,,,,"unweighted",,,,,,"weighted"'
   echo ',true,,7,,,30,,,7,,,30,,,,true,,7,,,30,,,7,,,30,'
   echo ',mean,,mean,stdev,,mean,stdev,,mean,stdev,,meand,stdev,,,mean,,mean,stdev,,mean,stdev,,mean,stdev,,meand,stdev'
 
   for income in "flat" "bbb"; do
-    sum=${summary/flat/$income}
     for resample in "nr" "r"; do
-      sum=${sum/nr/$resample}
       for pop in "vanilla" "popdens"; do
-        sum=${sum/vanilla/$pop}
 
         # print the population's true mean
         if [ "nr" == $resample ]; then
@@ -26,13 +24,16 @@ for summary in *_sample/vanilla.flat.nr*07*.txt; do
           else
             echo -n 'na,'
           fi
+          echo -n "no-resample,"
         else
-          echo -n ',,'
+          echo -n ',,resample,'
         fi
-        echo -n ','
 
         for prevalence in "prevalence" "weighted prevalence"; do
           for size in "07" "30"; do
+            sum=${summary/flat/$income}
+            sum=${sum/nr/$resample}
+            sum=${sum/vanilla/$pop}
             sum=${sum/07/$size}
             if [ -e $sum ]; then
               grep "^sampled child count:" $sum |
